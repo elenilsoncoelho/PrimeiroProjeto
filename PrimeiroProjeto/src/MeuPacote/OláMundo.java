35b96746a6d2eb3e9e155333d2ca90e370ec6726
elenilsoncoelho <106350019+elenilsoncoelho@users.noreply.github.com>
2022-10-21 11:03:58 -0300

    Initial commit

---
+++ PrimeiroProjeto/src/MeuPacote/OláMundo.java
@@ -0,0 +1,8 @@
+package MeuPacote;
+
+public class OláMundo {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+        System.out.println("Elenilson");
+    }
+}
